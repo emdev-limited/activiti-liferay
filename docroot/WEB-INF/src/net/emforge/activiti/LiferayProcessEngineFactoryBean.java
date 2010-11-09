d5f94ed0a2b8b85569b0fe75d4d9ba4977ef41b3
Alexey Kakunin <akakunin@gmail.com>
2010-11-09 23:56:35 +0000

    Added support for DB autodetection
    git-svn-id: http://svn.emforge.net/svnroot/activiti-liferay/trunk/activiti-web@13 45b537be-f2b2-426e-83e7-d28fc8d39b23

--- docroot/WEB-INF/src/net/emforge/activiti/LiferayProcessEngineFactoryBean.java
+++ docroot/WEB-INF/src/net/emforge/activiti/LiferayProcessEngineFactoryBean.java
@@ -1,6 +1,9 @@
 package net.emforge.activiti;
 
+import java.sql.Connection;
 import java.util.Map;
+
+import javax.sql.DataSource;
 
 import net.emforge.activiti.identity.LiferayIdentitySessionFactory;
 
@@ -9,6 +12,10 @@ import org.activiti.engine.impl.cfg.IdentitySession;
 import org.activiti.engine.impl.cfg.ProcessEngineConfiguration;
 import org.activiti.engine.impl.interceptor.SessionFactory;
 import org.activiti.spring.ProcessEngineFactoryBean;
+import org.apache.commons.lang.StringUtils;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 /** Custom implementation of ProcessEngineFactoryBean to set own IdentitySession
  * and customize other things
@@ -17,6 +24,8 @@ import org.activiti.spring.ProcessEngineFactoryBean;
  *
  */
 public class LiferayProcessEngineFactoryBean extends ProcessEngineFactoryBean {
+	private static Log _log = LogFactoryUtil.getLog(LiferayProcessEngineFactoryBean.class);
+	
 	@Override
 	public ProcessEngine getObject() throws Exception {
 		// preconfigure process engine to use our identity session
@@ -28,6 +37,50 @@ public class LiferayProcessEngineFactoryBean extends ProcessEngineFactoryBean {
 		
 		// Add Liferay Script Engine Factory
 		processEngineConfiguration.getScriptingEngines().addScriptEngineFactory(new LiferayScriptEngineFactory());
+		
+		// autodetect db type
+		if (StringUtils.isEmpty(processEngineConfiguration.getDatabaseType()) ||
+				processEngineConfiguration.getDatabaseType().equals("h2")) { // unfortunatelly Activiti always set it to h2 by default
+			autodetectDbType();
+		}
+		
 		return super.getObject();
+	}
+
+	private void autodetectDbType() {
+		_log.info("database type is not specified - try to autodetect it");
+		
+		DataSource dataSource = processEngineConfiguration.getDataSource();
+		
+		try {
+			Connection con = dataSource.getConnection();
+			try {
+				String dbName = con.getMetaData().getDatabaseProductName();
+				String databaseType = null;
+				
+				_log.info("DBName: " + dbName);
+				
+				if ("MySQL".equalsIgnoreCase(dbName)) {
+					databaseType = "mysql";
+				} else if ("Oracle".equalsIgnoreCase(dbName)) {
+					databaseType = "mysql";
+				} else if ("HSQL Database Engine".equalsIgnoreCase(dbName)) { // need to clarify about names for hsql and h2 as well as same type may be used for them
+					databaseType = "h2";
+				} else if ("PostgreSQL".equalsIgnoreCase(dbName)) {
+					databaseType = "postgres";
+				} else {
+					_log.warn("Unknown DB Type: " + dbName);
+				}
+				
+				if (databaseType != null) {
+					_log.info("Set Database Type to " + databaseType);
+					processEngineConfiguration.setDatabaseType(databaseType);
+				}
+			} finally {
+				con.close();
+			}
+		} catch (Exception ex) {
+			_log.error("Cannot autodetect connection", ex);
+		}
 	}
 }
