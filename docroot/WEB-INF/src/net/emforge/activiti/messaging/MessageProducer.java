a773bd2cebc544a60c97562b3688c38913ebab4b
Andrey Filippov <andrey.v.filippov@gmail.com>
2012-02-21 08:10:38 +0000

    Scan file attachments capability added
    git-svn-id: http://svn.emforge.net/svnroot/activiti-liferay/trunk/activiti-web@78 45b537be-f2b2-426e-83e7-d28fc8d39b23

--- docroot/WEB-INF/src/net/emforge/activiti/messaging/MessageProducer.java
+++ docroot/WEB-INF/src/net/emforge/activiti/messaging/MessageProducer.java
@@ -2,25 +2,41 @@ package net.emforge.activiti.messaging;
 
 import java.io.Serializable;
 
+import org.activiti.engine.delegate.DelegateExecution;
 import org.springframework.stereotype.Service;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.kernel.workflow.WorkflowInstanceManagerUtil;
+import com.liferay.portal.model.WorkflowInstanceLink;
+import com.liferay.portal.service.WorkflowInstanceLinkLocalServiceUtil;
 
 @Service("messageProducer")
-public class MessageProducer implements MessageProducerIF {
+public class MessageProducer {
 	private static Log _log = LogFactoryUtil.getLog(MessageProducer.class);
 	
-	@Override
-	public void sendMessage(Serializable classPK, String actionName,
-			String listenerName) throws Exception {
+	public void sendMessage(Serializable classPK, String actionName, String listenerName) throws Exception {
 		_log.debug("Sending message for listener [" + listenerName + "], classPK = [" + classPK + "], action name = [" + actionName + "]");
 		Message message = new Message();
 		message.put("actionName", actionName);
 		message.put("classPK", classPK);
 		MessageBusUtil.sendMessage(listenerName, message);
+	}
+	
+	public String sendMessage(DelegateExecution execution, Serializable classPK, String actionName,
+			String listenerName, String...transitionValues) throws Exception {
+		_log.info("Sending message for listener [" + listenerName + "], classPK = [" + classPK + "], action name = [" + actionName + "]");
+		Message message = new Message();
+		message.put("actionName", actionName);
+		message.put("classPK", classPK);
+		
+		String reply = (String) MessageBusUtil.sendSynchronousMessage(listenerName, message);
+		_log.info("Reply fetched [" + reply + "]");
+		return reply;
 	}
 
 	
