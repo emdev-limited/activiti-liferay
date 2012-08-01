d3b22a0a8af6dea7e852ed5dbc4ce9e7e0e04418
Andrey Filippov <andrey.v.filippov@gmail.com>
2012-08-01 17:10:13 +0000

    Using sequence flow name to create expression automatically
    git-svn-id: http://svn.emforge.net/svnroot/activiti-liferay/trunk/activiti-web@88 45b537be-f2b2-426e-83e7-d28fc8d39b23

--- docroot/WEB-INF/src/org/activiti/engine/impl/bpmn/parser/LiferayBpmnParse.java
+++ docroot/WEB-INF/src/org/activiti/engine/impl/bpmn/parser/LiferayBpmnParse.java
@@ -7,17 +7,20 @@ import java.util.Map;
 import net.emforge.activiti.hook.LiferayMailActivityBehavior;
 
 import org.activiti.engine.delegate.ExecutionListener;
+import org.activiti.engine.impl.Condition;
 import org.activiti.engine.impl.bpmn.behavior.EventBasedGatewayActivityBehavior;
 import org.activiti.engine.impl.bpmn.behavior.IntermediateCatchEventActivitiBehaviour;
 import org.activiti.engine.impl.bpmn.behavior.UserTaskActivityBehavior;
 import org.activiti.engine.impl.bpmn.helper.ClassDelegate;
 import org.activiti.engine.impl.bpmn.listener.ExpressionExecutionListener;
+import org.activiti.engine.impl.el.UelExpressionCondition;
 import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.activiti.engine.impl.pvm.process.ActivityImpl;
 import org.activiti.engine.impl.pvm.process.ScopeImpl;
 import org.activiti.engine.impl.pvm.process.TransitionImpl;
 import org.activiti.engine.impl.task.TaskDefinition;
 import org.activiti.engine.impl.util.xml.Element;
+import org.apache.commons.lang.StringUtils;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -26,6 +29,7 @@ public class LiferayBpmnParse extends BpmnParse {
 	private static Log _log = LogFactoryUtil.getLog(LiferayBpmnParse.class);
 	
 	protected Map<String,Element> userTaskElements = new HashMap<String,Element>();
+	protected Map<String,Element> exclusiveGatewayElements = new HashMap<String,Element>();
 
 	public LiferayBpmnParse(BpmnParser parser) { 
 		super(parser);
@@ -50,6 +54,18 @@ public class LiferayBpmnParse extends BpmnParse {
 		
 		return super.parseUserTask(userTaskElement, scope);
 	}
+	
+	/**
+	   * Parses an exclusive gateway declaration.
+	   */
+	  public ActivityImpl parseExclusiveGateway(Element exclusiveGwElement, ScopeImpl scope) {
+		String id = exclusiveGwElement.attribute("id");
+		if (id == null) {
+		     addError("Invalid property usage on line " + exclusiveGwElement.getLine() + ": no id or name specified.", exclusiveGwElement);
+		}
+		exclusiveGatewayElements.put(id, exclusiveGwElement);
+		return super.parseExclusiveGateway(exclusiveGwElement, scope);
+	  }
 	
 	/**
 	   * Parses all sequence flow of a scope.
@@ -95,7 +111,7 @@ public class LiferayBpmnParse extends BpmnParse {
 	        }
 	        
 	        if (userTaskElements.containsKey(destinationRef)) {
-		    	  _log.debug("Found appropriate targe user task");
+		    	  _log.debug("Found appropriate target user task");
 		    	  ExecutionListener executionListener = new ExpressionExecutionListener(expressionManager.createExpression("#{execution.removeVariable(\"outputTransition\")}"));
 		    	  if (executionListener != null) {
 		              // Since a transition only fires event 'take', we don't parse the
@@ -104,6 +120,42 @@ public class LiferayBpmnParse extends BpmnParse {
 		            }
 		    }
 	      } 
+	    }
+	  }
+	
+	/**
+	   * Parses a condition expression on a sequence flow.
+	   * 
+	   * @param seqFlowElement
+	   *          The 'sequenceFlow' element that can contain a condition.
+	   * @param seqFlow
+	   *          The sequenceFlow object representation to which the condition must
+	   *          be added.
+	   */
+	  public void parseSequenceFlowConditionExpression(Element seqFlowElement, TransitionImpl seqFlow) {
+	    Element conditionExprElement = seqFlowElement.element("conditionExpression");
+	    if (conditionExprElement != null) {
+	      String expression = conditionExprElement.getText().trim();
+	      String type = conditionExprElement.attributeNS(BpmnParser.XSI_NS, "type");
+	      if (type != null && !type.equals("tFormalExpression")) {
+	        addError("Invalid type, only tFormalExpression is currently supported", conditionExprElement);
+	      }
+
+	      Condition expressionCondition = new UelExpressionCondition(expressionManager.createExpression(expression));
+	      seqFlow.setProperty(PROPERTYNAME_CONDITION_TEXT, expression);
+	      seqFlow.setProperty(PROPERTYNAME_CONDITION, expressionCondition);
+	    } else {
+	    	_log.debug("Found appropriate source Exclusive gateway");
+	    	//Get label from sequence flow and use it in condition expression
+	    	String id = seqFlowElement.attribute("id");
+	    	String sourceRef = seqFlowElement.attribute("sourceRef");
+		    if (exclusiveGatewayElements.containsKey(sourceRef)) {
+		    	String name = seqFlowElement.attribute("name");
+		    	String expression = "${outputTransition == \"" + name + "\"}";
+		    	Condition expressionCondition = new UelExpressionCondition(expressionManager.createExpression(expression));
+			    seqFlow.setProperty(PROPERTYNAME_CONDITION_TEXT, expression);
+			    seqFlow.setProperty(PROPERTYNAME_CONDITION, expressionCondition);
+		    }
 	    }
 	  }
 
