d7ca3807e86355a8bf48f56e8e7dc4e703b1dc7e
Alexey Kakunin <akakunin@gmail.com>
2012-09-15 21:17:11 +0000

    Implemented image generation for process and task
    git-svn-id: http://svn.emforge.net/svnroot/activiti-liferay/trunk/activiti-web@129 45b537be-f2b2-426e-83e7-d28fc8d39b23

--- src/main/java/net/emforge/activiti/servlet/ImageServlet.java
+++ src/main/java/net/emforge/activiti/servlet/ImageServlet.java
@@ -3,17 +3,25 @@ package net.emforge.activiti.servlet;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.emforge.activiti.IdMappingService;
 import net.emforge.activiti.WorkflowDefinitionManagerImpl;
 import net.emforge.activiti.entity.WorkflowDefinitionExtensionImpl;
 
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.RepositoryServiceImpl;
+import org.activiti.engine.impl.bpmn.diagram.ProcessDiagramGenerator;
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.apache.commons.io.IOUtils;
 import org.springframework.context.ApplicationContext;
@@ -31,6 +39,11 @@ import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
+/** Generates images - most code samples got from ProcessDefinitionImageStreamResourceBuilder class from activiti-explorer application
+ * 
+ * @author akakunin
+ *
+ */
 public class ImageServlet extends HttpServlet {
 	private static final long serialVersionUID = 532117496619322018L;
     private static Log _log = LogFactoryUtil.getLog(ImageServlet.class);
@@ -42,41 +55,18 @@ public class ImageServlet extends HttpServlet {
 		String workflowName = request.getParameter("workflow");
 		Integer workflowVersion = GetterUtil.getInteger(request.getParameter("version"), 0);
 		Long workflowTaskId = GetterUtil.getLong(request.getParameter("taskId"), 0l);
+		Long processId = GetterUtil.getLong(request.getParameter("processId"), 0l);
 
-		// get application context
-		ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
-
-		// get beans
-		WorkflowDefinitionManagerImpl workflowManager = (WorkflowDefinitionManagerImpl)applicationContext.getBean("workflowDefinitionManager");
-		RepositoryService repositoryService = (RepositoryService)applicationContext.getBean(RepositoryService.class);
-		TaskService taskService = (TaskService)applicationContext.getBean(TaskService.class);
-		_log.debug("Get app context and beans");
-
-		String defId = null;
-		if (workflowTaskId > 0) {
-			_log.info("Generate image for task: " + workflowTaskId);
-			String taskId = String.valueOf(workflowTaskId);
-			Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
-			
-			defId = task.getProcessDefinitionId();
+		InputStream is = null;
+		if (workflowTaskId > 0l) {
+			is = getTaskImage(workflowTaskId);
+		} else if (processId > 0l) {
+			is = getProcessImage(processId);
 		} else {
-			_log.debug("Generate image for workflow " + workflowName + " varsion " + workflowVersion);
-		
-			// get workflow definition
-			WorkflowDefinitionExtensionImpl defExt = workflowManager.getWorkflowDefinitionExt(companyId, workflowName, workflowVersion);
-			
-			_log.debug("WorkflowDefExt: " + defExt);
-		
-			if (defExt != null) {
-				defId = defExt.getProcessDefinitionId();
-			}
+			is = getWorkflowImage(companyId, workflowName, workflowVersion);
 		}
 		
-		_log.info("Definition Id: " + defId);
-		
-		if (defId != null) {
-			// get Activiti Workflow
-			InputStream is = repositoryService.getProcessDiagram(defId);
+		if (is != null) {
 			byte[] binaryContent = IOUtils.toByteArray(is);
 			
 			_log.debug("Image Size: " + binaryContent.length);
@@ -89,6 +79,90 @@ public class ImageServlet extends HttpServlet {
 		} else {
 			super.doGet(request, response);
 		}
+	}
+	
+	protected InputStream getTaskImage(Long workflowTaskId) {
+		_log.debug("Generate image for task: " + workflowTaskId);
+		
+		// get application context
+		ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
+
+		// get beans
+		TaskService taskService = (TaskService)applicationContext.getBean(TaskService.class);
+		RepositoryService repositoryService = (RepositoryService)applicationContext.getBean(RepositoryService.class);
+		_log.debug("Get app context and beans");
+
+		
+		String taskId = String.valueOf(workflowTaskId);
+		Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
+
+		List<String> taskIds = new ArrayList<String>();
+		taskIds.add(task.getTaskDefinitionKey());
+		
+		String defId = task.getProcessDefinitionId();
+		
+		// it is important to use this way to get ProcessDEfinition - since in this case readed all required for image generation data
+		ProcessDefinitionEntity processDefinition = (ProcessDefinitionEntity)(ProcessDefinitionEntity) ((RepositoryServiceImpl) repositoryService).getDeployedProcessDefinition(defId);
+		
+		return ProcessDiagramGenerator.generateDiagram(processDefinition, "png", taskIds);
+	}
+	
+	protected InputStream getProcessImage(Long processInstanceId) {
+		
+		// get application context
+		ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
+
+		// get beans
+		IdMappingService idMappingService = (IdMappingService)applicationContext.getBean(IdMappingService.class);
+		RuntimeService runtimeService = (RuntimeService)applicationContext.getBean(RuntimeService.class);
+		RepositoryService repositoryService = (RepositoryService)applicationContext.getBean(RepositoryService.class);
+		_log.debug("Get app context and beans");
+		
+		String procId = idMappingService.getActivitiProcessInstanceId(processInstanceId);
+		if (procId == null) {
+			procId = String.valueOf(processInstanceId);
+		}
+		ProcessInstance inst = runtimeService.createProcessInstanceQuery().processInstanceId(procId).singleResult();
+		
+	    ProcessDefinitionEntity processDefinition = (ProcessDefinitionEntity) ((RepositoryServiceImpl) repositoryService).getDeployedProcessDefinition(inst.getProcessDefinitionId());
+
+	    if (processDefinition != null && processDefinition.isGraphicalNotationDefined()) {
+	    	return ProcessDiagramGenerator.generateDiagram(processDefinition, "png", runtimeService.getActiveActivityIds(inst.getId()));
+	    } 
+	    
+		return null;
+	}
+	
+	protected InputStream getWorkflowImage(Long companyId, String workflowName, Integer workflowVersion) {
+		String defId = null;
+		
+		_log.debug("Generate image for workflow " + workflowName + " version " + workflowVersion);
+
+		// get application context
+		ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
+
+		// get beans
+		WorkflowDefinitionManagerImpl workflowManager = (WorkflowDefinitionManagerImpl)applicationContext.getBean("workflowDefinitionManager");
+		RepositoryService repositoryService = (RepositoryService)applicationContext.getBean(RepositoryService.class);
+		_log.debug("Get app context and beans");
+		
+		// get workflow definition
+		WorkflowDefinitionExtensionImpl defExt = workflowManager.getWorkflowDefinitionExt(companyId, workflowName, workflowVersion);
+		
+		_log.debug("WorkflowDefExt: " + defExt);
+	
+		if (defExt != null) {
+			defId = defExt.getProcessDefinitionId();
+		}
+	
+		_log.debug("Definition Id: " + defId);
+	
+		if (defId != null) {
+			// get Process Definition Diagram 
+			return repositoryService.getProcessDiagram(defId);
+		}
+
+		return null;
 	}
 	
 	public void sendFile(
