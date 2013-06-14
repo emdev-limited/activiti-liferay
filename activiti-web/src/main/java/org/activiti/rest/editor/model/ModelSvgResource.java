6c49ac4fbc5edb4c6ca93b3087441c2507a4bb56
Alexey Kakunin <akakunin@gmail.com>
2013-06-14 20:58:28 +0400

    avoided non-required dependencies. Removed extra imports

--- activiti-web/src/main/java/org/activiti/rest/editor/model/ModelSvgResource.java
+++ activiti-web/src/main/java/org/activiti/rest/editor/model/ModelSvgResource.java
@@ -1,37 +1,16 @@
 package org.activiti.rest.editor.model;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-
-import net.emforge.activiti.service.ActivitiLocalServiceUtil;
 
 import org.activiti.engine.ActivitiException;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
-import org.activiti.engine.impl.RepositoryServiceImpl;
-import org.activiti.engine.impl.cmd.SaveResourceCmd;
-import org.activiti.engine.impl.persistence.entity.ResourceEntity;
-import org.activiti.engine.repository.Model;
-import org.activiti.rest.api.ActivitiUtil;
 import org.activiti.rest.api.SecuredResource;
 import org.activiti.rest.api.exceptions.NotFoundException;
-import org.activiti.rest.api.model.ModelResponse;
-import org.apache.batik.transcoder.TranscoderInput;
-import org.apache.batik.transcoder.TranscoderOutput;
-import org.apache.batik.transcoder.image.PNGTranscoder;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.node.ObjectNode;
 import org.restlet.data.MediaType;
 import org.restlet.representation.InputRepresentation;
-import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
-import org.restlet.resource.Put;
-
-import com.liferay.portal.kernel.util.StringPool;
 
 public class ModelSvgResource extends SecuredResource {
 
