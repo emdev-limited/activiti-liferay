933674257a3d88eb92ea6b26ee98695914671e67
fav <afilippov@emdev.ru>
2013-09-05 11:52:05 +0400

    Cherry-picked https://bitbucket.org/emdev-limited/activiti-liferay/commits/dec7af5dd86ed86eb279173ce7fb51c5374b1493

--- activiti-web/src/main/java/net/emforge/activiti/service/impl/ActivitiLocalServiceImpl.java
+++ activiti-web/src/main/java/net/emforge/activiti/service/impl/ActivitiLocalServiceImpl.java
@@ -5,9 +5,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.activiti.engine.RuntimeService;
-import org.activiti.engine.TaskService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -22,155 +19,179 @@ import net.emforge.activiti.spring.ApplicationContextProvider;
 
 /**
  * The implementation of the activiti local service.
- *
+ * 
  * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link net.emforge.activiti.service.ActivitiLocalService} interface.
- *
+ * All custom service methods should be put in this class. Whenever methods are
+ * added, rerun ServiceBuilder to copy their definitions into the
+ * {@link net.emforge.activiti.service.ActivitiLocalService} interface.
+ * 
  * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM.
  * </p>
- *
+ * 
  * @author Brian Wing Shun Chan
  * @see net.emforge.activiti.service.base.ActivitiLocalServiceBaseImpl
  * @see net.emforge.activiti.service.ActivitiLocalServiceUtil
  */
 public class ActivitiLocalServiceImpl extends ActivitiLocalServiceBaseImpl {
-	
-	@Autowired
-	TaskService taskService;
-	
-	@Autowired
-	RuntimeService runtimeService;	
-	
+
     /*
      * NOTE FOR DEVELOPERS:
-     *
-     * Never reference this interface directly. Always use {@link net.emforge.activiti.service.ActivitiLocalServiceUtil} to access the activiti local service.
+     * 
+     * Never reference this interface directly. Always use {@link
+     * net.emforge.activiti.service.ActivitiLocalServiceUtil} to access the
+     * activiti local service.
      */
-	private static Log _log = LogFactoryUtil.getLog(ActivitiLocalServiceImpl.class.getName());
-	
-	@Override
-	public String createNewModel(String modelName, String modelDescription) throws SystemException, PortalException {
-		try {
-			ApplicationContext context = ApplicationContextProvider.getApplicationContext();
-			ActivitiTransactionHelperIF helper = (ActivitiTransactionHelperIF) context.getBean("activitiTransactionHelper");
-	        return helper.createNewModel(modelName, modelDescription);  
-	    } catch(Exception e) {
-	      _log.error(e,e);
-	      throw new RuntimeException(e.getMessage());
-	    }
-	}
-	
-	@Override
-	public String test(String s) throws SystemException, PortalException {
-		
-		try {
-			ApplicationContext context = ApplicationContextProvider.getApplicationContext();
-			ActivitiTransactionHelperIF helper = (ActivitiTransactionHelperIF) context.getBean("activitiTransactionHelper");
-			return helper.test(s);
-		} catch (Exception e) {
-			_log.error(e,e);
-			throw new RuntimeException(e.getMessage());
-		}
-	}
-	
-	/**
-	 * Returns all execution ids, including sub-process executions
-	 * @param instanceIds
-	 * @return
-	 * @throws SystemException
-	 */
-	@Override
-	public List<String> findAllExecutions(List instanceIds) throws SystemException {
-		// convert List<Long> to List<String>
-		ArrayList<String> lstInstIds = new ArrayList<String>(instanceIds.size()); 
-		for (Object instanceId: instanceIds)
-			lstInstIds.add(instanceId.toString());
-		
-		// find all executions, including sub-executions
-		List<String> topExecutions = ActivitiFinderUtil.findTopExecutions(lstInstIds);
-		ArrayList<String> allExecutions = new ArrayList<String>(topExecutions.size()*2);
-		allExecutions.addAll(topExecutions);
-		
-		while (true) {
-			List<String> subExecutions = ActivitiFinderUtil.findSubExecutions(topExecutions);
-			if (subExecutions == null || subExecutions.size() == 0) {
-				break;
-			}
-			allExecutions.addAll(subExecutions);
-			topExecutions = subExecutions;
-		}
-		return allExecutions;
-	}
-	
-	/**
-	 * Returns active UserTask names for selected instances.
-	 * @param instanceIds
-	 * @return
-	 * @throws SystemException 
-	 */
-	@Override
-	public Set<String> findUniqueUserTaskNames(List<String> executionIds) throws SystemException {
-		// find uniqie task names
-		List<String> lstTasks = ActivitiFinderUtil.findUniqueUserTaskNames(executionIds);
-		HashSet<String> res = new HashSet<String>(lstTasks.size());
-		res.addAll(lstTasks);
+    private static Log _log = LogFactoryUtil.getLog(ActivitiLocalServiceImpl.class.getName());
 
-		return res;
-	}
+    @Override
+    public String createNewModel(String modelName, String modelDescription)
+            throws SystemException, PortalException {
+        try {
+            ApplicationContext context = ApplicationContextProvider
+                    .getApplicationContext();
+            ActivitiTransactionHelperIF helper = (ActivitiTransactionHelperIF) context
+                    .getBean("activitiTransactionHelper");
+            return helper.createNewModel(modelName, modelDescription);
+        } catch (Exception e) {
+            _log.error(e, e);
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
-	/**
-	 * Returns active UserTask assignees for selected instances.
-	 * @param instanceIds
-	 * @return
-	 */	
-	@Override
-	public Set findUniqueUserTaskAssignees(List<String> executionIds) throws SystemException {
-		// find uniqie assignees
-		List lstAsg = ActivitiFinderUtil.findUniqueUserTaskAssignees(executionIds);
-		HashSet res = new HashSet(lstAsg.size());
-		res.addAll(lstAsg);
+    @Override
+    public String test(String s) throws SystemException, PortalException {
 
-		return res;		
-	}	
-	
-	/**
-	 * Returns top level process instances, filtered by active user task. 
-	 * @param taskName - user task name
-	 * @param assigneeUser - task assignee
-	 * @param candidateRole - candidate role for task
-	 * @return
-	 * @throws SystemException 
-	 */
-	@Override
-	public List<String> findTopLevelProcessInstances(String taskName, String assigneeUser, String candidateRole) throws SystemException {
-		
-		List<Object[]> lstExec = ActivitiFinderUtil.findUserTasks(taskName, assigneeUser, candidateRole);
-		if (lstExec.size() == 0)
-			return new ArrayList<String>(0);
-		
-		ArrayList<String> lstInstances = new ArrayList<String>(lstExec.size() * 2);
-		extractColumn(lstExec, 1, lstInstances);
-		
-		ArrayList<String> lstSuperExec =  new ArrayList<String>(lstInstances.size());
-		extractColumn(lstExec, 2, lstSuperExec);
-		
-		while (lstSuperExec.size() > 0) {
-			lstExec = ActivitiFinderUtil.findSuperExecutions(lstSuperExec);
-			extractColumn(lstExec, 1, lstInstances);
-			
-			lstSuperExec.clear();
-			extractColumn(lstExec, 2, lstSuperExec);
-		}
-		
-		return lstInstances;
-	}
-	
-	private void extractColumn(List<Object[]> source, int ncol, List dest) {
-		for (Object[] row: source) {
-			if (row[ncol] != null) {
-				dest.add(row[ncol]);		
-			}
-		}
-	}
+        try {
+            ApplicationContext context = ApplicationContextProvider
+                    .getApplicationContext();
+            ActivitiTransactionHelperIF helper = (ActivitiTransactionHelperIF) context
+                    .getBean("activitiTransactionHelper");
+            return helper.test(s);
+        } catch (Exception e) {
+            _log.error(e, e);
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * Returns all execution ids, including sub-process executions
+     * 
+     * @param instanceIds
+     * @return
+     * @throws SystemException
+     */
+    @Override
+    public List<String> findAllExecutions(List instanceIds)
+            throws SystemException {
+        // convert List<Long> to List<String>
+        ArrayList<String> lstInstIds = new ArrayList<String>(instanceIds.size());
+        for (Object instanceId : instanceIds)
+            lstInstIds.add(instanceId.toString());
+
+        // find all executions, including sub-executions
+        List<String> topExecutions = ActivitiFinderUtil
+                .findTopExecutions(lstInstIds);
+        ArrayList<String> allExecutions = new ArrayList<String>(
+                topExecutions.size() * 2);
+        allExecutions.addAll(topExecutions);
+
+        while (true) {
+            List<String> subExecutions = ActivitiFinderUtil
+                    .findSubExecutions(topExecutions);
+            if (subExecutions == null || subExecutions.size() == 0) {
+                break;
+            }
+            allExecutions.addAll(subExecutions);
+            topExecutions = subExecutions;
+        }
+        return allExecutions;
+    }
+
+    /**
+     * Returns active UserTask names for selected instances.
+     * 
+     * @param instanceIds
+     * @return
+     * @throws SystemException
+     */
+    @Override
+    public Set<String> findUniqueUserTaskNames(List<String> executionIds)
+            throws SystemException {
+        // find uniqie task names
+        List<String> lstTasks = ActivitiFinderUtil
+                .findUniqueUserTaskNames(executionIds);
+        HashSet<String> res = new HashSet<String>(lstTasks.size());
+        res.addAll(lstTasks);
+
+        return res;
+    }
+
+    /**
+     * Returns active UserTask assignees for selected instances.
+     * 
+     * @param instanceIds
+     * @return
+     */
+    @Override
+    public Set findUniqueUserTaskAssignees(List<String> executionIds)
+            throws SystemException {
+        // find uniqie assignees
+        List lstAsg = ActivitiFinderUtil
+                .findUniqueUserTaskAssignees(executionIds);
+        HashSet res = new HashSet(lstAsg.size());
+        res.addAll(lstAsg);
+
+        return res;
+    }
+
+    /**
+     * Returns top level process instances, filtered by active user task.
+     * 
+     * @param taskName
+     *            - user task name
+     * @param assigneeUser
+     *            - task assignee
+     * @param candidateRole
+     *            - candidate role for task
+     * @return
+     * @throws SystemException
+     */
+    @Override
+    public List<String> findTopLevelProcessInstances(String taskName,
+            String assigneeUser, String candidateRole) throws SystemException {
+
+        List<Object[]> lstExec = ActivitiFinderUtil.findUserTasks(taskName,
+                assigneeUser, candidateRole);
+        if (lstExec.size() == 0)
+            return new ArrayList<String>(0);
+
+        ArrayList<String> lstInstances = new ArrayList<String>(
+                lstExec.size() * 2);
+        extractColumn(lstExec, 1, lstInstances);
+
+        ArrayList<String> lstSuperExec = new ArrayList<String>(
+                lstInstances.size());
+        extractColumn(lstExec, 2, lstSuperExec);
+
+        while (lstSuperExec.size() > 0) {
+            lstExec = ActivitiFinderUtil.findSuperExecutions(lstSuperExec);
+            extractColumn(lstExec, 1, lstInstances);
+
+            lstSuperExec.clear();
+            extractColumn(lstExec, 2, lstSuperExec);
+        }
+
+        return lstInstances;
+    }
+
+    private void extractColumn(List<Object[]> source, int ncol, List dest) {
+        for (Object[] row : source) {
+            if (row[ncol] != null) {
+                dest.add(row[ncol]);
+            }
+        }
+    }
 }
--- activiti-web/src/main/java/net/emforge/activiti/service/persistence/ActivitiFinderImpl.java
+++ activiti-web/src/main/java/net/emforge/activiti/service/persistence/ActivitiFinderImpl.java
@@ -16,169 +16,182 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.service.persistence.impl.BasePersistenceImpl;
 import com.liferay.util.dao.orm.CustomSQLUtil;
 
-public class ActivitiFinderImpl extends BasePersistenceImpl implements ActivitiFinder {
-	
-	private static Log log = LogFactoryUtil.getLog(ActivitiFinderImpl.class);
-	
-	private static final String FIND_TOP_EXECUTIONS = ActivitiFinderImpl.class.getName() + ".findTopExecutions";	
-	private static final String FIND_SUB_EXECUTIONS = ActivitiFinderImpl.class.getName() + ".findSubExecutions";
-	private static final String FIND_UNIQUE_TASK_NAMES = ActivitiFinderImpl.class.getName() + ".findUniqueUserTaskNames";
-	private static final String FIND_UNIQUE_TASK_ASSIGNEES = ActivitiFinderImpl.class.getName() + ".findUniqueUserTaskAssignees";
-	private static final String FIND_USER_TASKS = ActivitiFinderImpl.class.getName() + ".findUserTasks";
-	private static final String FIND_SUPER_EXECUTIONS = ActivitiFinderImpl.class.getName() + ".findSuperExecutions";
-	
-	private static final String FIND_USER_TASKS_TASKNAME = " and (t.NAME_ = ?)";
-	private static final String FIND_USER_TASKS_ASSIGNEE = " and (t.ASSIGNEE_ = ?)";
-	private static final String FIND_USER_TASKS_CANDIDATE = " and (l.GROUP_ID_ = ?)";
+public class ActivitiFinderImpl extends BasePersistenceImpl implements
+        ActivitiFinder {
 
-	@Override
-	public List<String> findTopExecutions(List<String> processInstanceIds) throws SystemException {
+    private static Log log = LogFactoryUtil.getLog(ActivitiFinderImpl.class);
+
+    private static final String FIND_TOP_EXECUTIONS = ActivitiFinderImpl.class.getName() + ".findTopExecutions";
+    private static final String FIND_SUB_EXECUTIONS     = ActivitiFinderImpl.class.getName() + ".findSubExecutions";
+    private static final String FIND_UNIQUE_TASK_NAMES   = ActivitiFinderImpl.class.getName() + ".findUniqueUserTaskNames";
+    private static final String FIND_UNIQUE_TASK_ASSIGNEES = ActivitiFinderImpl.class.getName() + ".findUniqueUserTaskAssignees";
+    private static final String FIND_USER_TASKS         = ActivitiFinderImpl.class.getName() + ".findUserTasks";
+    private static final String FIND_SUPER_EXECUTIONS     = ActivitiFinderImpl.class.getName() + ".findSuperExecutions";
+
+    private static final String FIND_USER_TASKS_TASKNAME   = " and (t.NAME_ = ?)";
+    private static final String FIND_USER_TASKS_ASSIGNEE   = " and (t.ASSIGNEE_ = ?)";
+    private static final String FIND_USER_TASKS_CANDIDATE  = " and (l.GROUP_ID_ = ?)";
+
+    @Override
+    public List<String> findTopExecutions(List<String> processInstanceIds)
+            throws SystemException {
         Session session = null;
         try {
-                session = openSession();
-                String sql = CustomSQLUtil.get(FIND_TOP_EXECUTIONS);
+            session = openSession();
+            String sql = CustomSQLUtil.get(FIND_TOP_EXECUTIONS);
 
-                String sgrp = "(" + StringUtils.join(processInstanceIds, ",") + ")";
-                sql = StringUtil.replace(sql, "[$INSTANCE_IDS$]", sgrp);                	
-                                
-                SQLQuery sqlQuery = session.createSQLQuery(sql);
-                
-                List<String> itemIds = (List<String>)QueryUtil.list(sqlQuery, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);                        
-                
-                return itemIds;                                 
+            String sgrp = "(" + StringUtils.join(processInstanceIds, ",") + ")";
+            sql = StringUtil.replace(sql, "[$INSTANCE_IDS$]", sgrp);
+
+            SQLQuery sqlQuery = session.createSQLQuery(sql);
+
+            List<String> itemIds = (List<String>) QueryUtil.list(sqlQuery,
+                    getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+            return itemIds;
         } catch (Exception e) {
-                throw new SystemException(e);
+            throw new SystemException(e);
         } finally {
-                closeSession(session);
-        }     			
-	}
-	
-	@Override
-	public List<String> findSubExecutions(List<String> execIds) throws SystemException {
+            closeSession(session);
+        }
+    }
+
+    @Override
+    public List<String> findSubExecutions(List<String> execIds)
+            throws SystemException {
         Session session = null;
         try {
-                session = openSession();
-                String sql = CustomSQLUtil.get(FIND_SUB_EXECUTIONS);
+            session = openSession();
+            String sql = CustomSQLUtil.get(FIND_SUB_EXECUTIONS);
 
-                String sgrp = "(" + StringUtils.join(execIds, ",") + ")";
-                sql = StringUtil.replace(sql, "[$EXEC_IDS$]", sgrp);                	
-                                
-                SQLQuery sqlQuery = session.createSQLQuery(sql);
-                
-                List<String> itemIds = (List<String>)QueryUtil.list(sqlQuery, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);                        
-                
-                return itemIds;                                 
+            String sgrp = "(" + StringUtils.join(execIds, ",") + ")";
+            sql = StringUtil.replace(sql, "[$EXEC_IDS$]", sgrp);
+
+            SQLQuery sqlQuery = session.createSQLQuery(sql);
+
+            List<String> itemIds = (List<String>) QueryUtil.list(sqlQuery,
+                    getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+            return itemIds;
         } catch (Exception e) {
-                throw new SystemException(e);
+            throw new SystemException(e);
         } finally {
-                closeSession(session);
-        }     			
-	}
-	
-	@Override
-	public List<String> findUniqueUserTaskNames(List<String> execIds) throws SystemException {
+            closeSession(session);
+        }
+    }
+
+    @Override
+    public List<String> findUniqueUserTaskNames(List<String> execIds)
+            throws SystemException {
         Session session = null;
         try {
-                session = openSession();
-                String sql = CustomSQLUtil.get(FIND_UNIQUE_TASK_NAMES);
+            session = openSession();
+            String sql = CustomSQLUtil.get(FIND_UNIQUE_TASK_NAMES);
 
-                String sgrp = "(" + StringUtils.join(execIds, ",") + ")";
-                sql = StringUtil.replace(sql, "[$EXEC_IDS$]", sgrp);                	
-                                
-                SQLQuery sqlQuery = session.createSQLQuery(sql);
-                
-                List<String> itemIds = (List<String>)QueryUtil.list(sqlQuery, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);                        
-                
-                return itemIds;                                 
+            String sgrp = "(" + StringUtils.join(execIds, ",") + ")";
+            sql = StringUtil.replace(sql, "[$EXEC_IDS$]", sgrp);
+
+            SQLQuery sqlQuery = session.createSQLQuery(sql);
+
+            List<String> itemIds = (List<String>) QueryUtil.list(sqlQuery,
+                    getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+            return itemIds;
         } catch (Exception e) {
-                throw new SystemException(e);
+            throw new SystemException(e);
         } finally {
-                closeSession(session);
-        }     			
-	}
-	
-	@Override
-	public List findUniqueUserTaskAssignees(List<String> execIds) throws SystemException {
+            closeSession(session);
+        }
+    }
+
+    @Override
+    public List findUniqueUserTaskAssignees(List<String> execIds)
+            throws SystemException {
         Session session = null;
         try {
-                session = openSession();
-                String sql = CustomSQLUtil.get(FIND_UNIQUE_TASK_ASSIGNEES);
+            session = openSession();
+            String sql = CustomSQLUtil.get(FIND_UNIQUE_TASK_ASSIGNEES);
 
-                String sgrp = "(" + StringUtils.join(execIds, ",") + ")";
-                sql = StringUtil.replace(sql, "[$EXEC_IDS$]", sgrp);                	
-                                
-                SQLQuery sqlQuery = session.createSQLQuery(sql);
-                
-                List itemIds = QueryUtil.list(sqlQuery, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);                        
-                
-                return itemIds;                                 
+            String sgrp = "(" + StringUtils.join(execIds, ",") + ")";
+            sql = StringUtil.replace(sql, "[$EXEC_IDS$]", sgrp);
+
+            SQLQuery sqlQuery = session.createSQLQuery(sql);
+
+            List itemIds = QueryUtil.list(sqlQuery, getDialect(),
+                    QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+            return itemIds;
         } catch (Exception e) {
-                throw new SystemException(e);
+            throw new SystemException(e);
         } finally {
-                closeSession(session);
-        }     			
-	}
-	
-	@Override
-	public List<Object[]> findUserTasks(String taskName, String assigneeUser, String candidateRole) throws SystemException {
-		
+            closeSession(session);
+        }
+    }
+
+    @Override
+    public List<Object[]> findUserTasks(String taskName, String assigneeUser,
+            String candidateRole) throws SystemException {
+
         Session session = null;
         try {
-                session = openSession();
-                String sql = CustomSQLUtil.get(FIND_USER_TASKS);
+            session = openSession();
+            String sql = CustomSQLUtil.get(FIND_USER_TASKS);
 
-                if (Validator.isNotNull(taskName)) {
-                	sql = sql + FIND_USER_TASKS_TASKNAME;
-                }
-                if (Validator.isNotNull(assigneeUser)) {
-                	sql = sql + FIND_USER_TASKS_ASSIGNEE;
-                }     
-                if (Validator.isNotNull(candidateRole)) {
-                	sql = sql + FIND_USER_TASKS_CANDIDATE;
-                }                 
-                                
-                SQLQuery sqlQuery = session.createSQLQuery(sql);
-                
-                QueryPos qPos = QueryPos.getInstance(sqlQuery);
-                if (Validator.isNotNull(taskName)) {
-                	qPos.add(taskName);
-                }
-                if (Validator.isNotNull(assigneeUser)) {
-                	qPos.add(assigneeUser);
-                }
-                if (Validator.isNotNull(candidateRole)) {
-                	qPos.add(candidateRole);
-                }
-                
-                List<Object[]> itemIds = (List<Object[]>)QueryUtil.list(sqlQuery, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);                        
-                
-                return itemIds;                                 
+            if (Validator.isNotNull(taskName)) {
+                sql = sql + FIND_USER_TASKS_TASKNAME;
+            }
+            if (Validator.isNotNull(assigneeUser)) {
+                sql = sql + FIND_USER_TASKS_ASSIGNEE;
+            }
+            if (Validator.isNotNull(candidateRole)) {
+                sql = sql + FIND_USER_TASKS_CANDIDATE;
+            }
+
+            SQLQuery sqlQuery = session.createSQLQuery(sql);
+
+            QueryPos qPos = QueryPos.getInstance(sqlQuery);
+            if (Validator.isNotNull(taskName)) {
+                qPos.add(taskName);
+            }
+            if (Validator.isNotNull(assigneeUser)) {
+                qPos.add(assigneeUser);
+            }
+            if (Validator.isNotNull(candidateRole)) {
+                qPos.add(candidateRole);
+            }
+
+            List<Object[]> itemIds = (List<Object[]>) QueryUtil.list(sqlQuery,
+                    getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+            return itemIds;
         } catch (Exception e) {
-                throw new SystemException(e);
+            throw new SystemException(e);
         } finally {
-                closeSession(session);
-        }     			
-	}
-	
-	@Override
-	public List<Object[]> findSuperExecutions(List<String> execIds) throws SystemException {
+            closeSession(session);
+        }
+    }
+
+    @Override
+    public List<Object[]> findSuperExecutions(List<String> execIds)
+            throws SystemException {
         Session session = null;
         try {
-                session = openSession();
-                String sql = CustomSQLUtil.get(FIND_SUPER_EXECUTIONS);
+            session = openSession();
+            String sql = CustomSQLUtil.get(FIND_SUPER_EXECUTIONS);
 
-                String sgrp = "(" + StringUtils.join(execIds, ",") + ")";
-                sql = StringUtil.replace(sql, "[$EXEC_IDS$]", sgrp);                	
-                                
-                SQLQuery sqlQuery = session.createSQLQuery(sql);
-                
-                List<Object[]> itemIds = (List<Object[]>)QueryUtil.list(sqlQuery, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);                        
-                
-                return itemIds;                                 
+            String sgrp = "(" + StringUtils.join(execIds, ",") + ")";
+            sql = StringUtil.replace(sql, "[$EXEC_IDS$]", sgrp);
+
+            SQLQuery sqlQuery = session.createSQLQuery(sql);
+
+            List<Object[]> itemIds = (List<Object[]>) QueryUtil.list(sqlQuery,
+                    getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+            return itemIds;
         } catch (Exception e) {
-                throw new SystemException(e);
+            throw new SystemException(e);
         } finally {
-                closeSession(session);
-        }  		
-	}
+            closeSession(session);
+        }
+    }
 }
