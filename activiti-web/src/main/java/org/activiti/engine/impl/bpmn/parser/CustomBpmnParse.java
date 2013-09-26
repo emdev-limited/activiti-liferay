0b5f055976c56813fdc5782c80a3bb5bf0101db9
fav <afilippov@emdev.ru>
2013-09-26 23:10:07 +0400

    Fixed transition name for a single output from a user task.

--- activiti-web/src/main/java/org/activiti/engine/impl/bpmn/parser/CustomBpmnParse.java
+++ activiti-web/src/main/java/org/activiti/engine/impl/bpmn/parser/CustomBpmnParse.java
@@ -56,7 +56,7 @@ public class CustomBpmnParse extends BpmnParse {
 				for (FormPostProcessorWrapper form : forms) {
 					List<String> outpuTransitionNames = form.getOutputTransitionNames();
 					if (form.getSourceActivity() != null && outpuTransitionNames != null 
-							&& !outpuTransitionNames.isEmpty() && outpuTransitionNames.size() > 1) {
+							&& !outpuTransitionNames.isEmpty()) {
 						try {
 							ActivityImpl userTaskActivity = form.getSourceActivity();
 							//get user task
